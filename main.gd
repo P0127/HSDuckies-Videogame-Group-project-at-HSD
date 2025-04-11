f1242af8e44e701d2eefb614e7b19e72b1cb377c
Tim_Moog <205991001+Tim-Moog@users.noreply.github.com>
2025-04-11 12:31:37 +0200

    First Mob Tests
    very basic Mob scene
    incl a basic method of spawning enemies using left click for future path finding testing n stuff
    -currently doesnt spawn at correct mouse position if player moves

--- main.gd
+++ main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var Mob: PackedScene
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +15,10 @@ func _process(delta: float) -> void:
 func new_game():
 	$Player.start($StartPosition.position)
 	#moves player to starting position & removed hide()
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		if Mob.can_instantiate():
+			var new_Mob = Mob.instantiate()
+			new_Mob.position = get_viewport().get_mouse_position()
+			add_child(new_Mob)
