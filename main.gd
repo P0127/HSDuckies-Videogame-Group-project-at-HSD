8b3dd4dcd6b7ad7c7fb3f599420603c5a9bef89a
Selina-Staender <selina.staender@outlook.de>
2025-04-14 16:56:17 +0200

    FINALLY
    Mobs, Player and Map work as intended!
    All new Functions work proberly

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var Mob: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,10 @@ func _process(delta: float) -> void:
 func new_game():
 	$Player.start($StartPosition.position)
 	#moves player to starting position & removed hide()
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		if Mob.can_instantiate():
+			var new_Mob = Mob.instantiate()
+			new_Mob.position = $Player.position + get_viewport().get_mouse_position() - Vector2($StartPosition.position)
+			add_child(new_Mob)
