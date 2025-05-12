33231bae12c7e03d7f4d8298e014de5e66bba563
Selina-Staender <selina.staender@outlook.de>
2025-05-12 16:26:54 +0200

    Added TEMPORARY tem pickup effects

--- drop_chocolatebar.gd
+++ drop_chocolatebar.gd
@@ -5,7 +5,8 @@ func _ready():
 	pass 
 
 func _on_body_entered(body):
+	print("Chocolatebar is collected!")
 	
-		print("Chocolatebar is collected!")
-		queue_free()  # Entfernt den Energy Drink aus der Szene
-		
+	queue_free()  # Entfernt den Energy Drink aus der Szene
+	GlobalSignals.boost_firerate_collected_signal.emit()#using a global script to get the signal
+	#to the hud without requiring it to be a child/parent node 
