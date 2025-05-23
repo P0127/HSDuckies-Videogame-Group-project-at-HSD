725a40d19c11b9bc43d56847ffe6d1f41f0ffa1d
Selina-Staender <selina.staender@outlook.de>
2025-05-23 13:38:13 +0200

    Systemfile Reorganized
    assets, scrips and scenes are now bunched together depending on their type

---
+++ Drops/pickable.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+# Implemented in every scene describing an object that can get picked up
+# Scenes this is used in need to implement the Method "pickup(Node:Player)"
+# Gets asked for by Body contact (is this child present?)
