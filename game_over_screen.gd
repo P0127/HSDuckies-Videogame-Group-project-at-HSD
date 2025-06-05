19aa8f47aecffbf7357acb54f5e3846d795c03e4
karin0127 <kpurginova@gmail.com>
2025-06-05 22:20:17 +0200

    Added menu button animation
    addes the menu button animation but the quality is not great yet ill see what i can or cant do about it

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -2,7 +2,15 @@ extends CanvasLayer
 
 
 @onready var menu_button = $MainMenuButton
+@onready var menu_button_anim = $MenuButtonAnimation
 
+signal game_over
+	
+func _ready() -> void:
+	$MenuButtonSpriteSheet	.frame = 0 
+	 #Show normal button frame
+	
+	
 # Closes the whole game
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
@@ -11,6 +19,12 @@ func _on_quit_button_pressed() -> void:
 # Loads the main scene which functions as main menu
 func _on_main_menu_button_pressed() -> void:
 	
+	menu_button_anim.play("pressed")
+	
+	#await menu_button_anim.animation_finished
+	await get_tree().create_timer(0.5).timeout
+	
+	game_over.emit()
 	get_tree().change_scene_to_file("res://main.tscn")
 	
 	
