6e61dbc7455f20e9631774f4c37c887d1943c199
miraytrm <miraytuerkmen@outlook.de>
2025-06-07 01:30:47 +0200

    sound for choco drop and energydrink drop

--- Drops/drop_chocolatebar.gd
+++ Drops/drop_chocolatebar.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var soundChocolatebar := $AudioStreamPlayer
+
 var effectChangeAmount = 2 #Halves Time needed to shoot
 var effectLength = 5
 
@@ -10,8 +12,16 @@ func pickup(player : Node2D):
 	#Calls Player Method to change weapon firerate
 	player.increase_firerate(effectChangeAmount)
 	
+	# plays Sound when picked up
+	if soundChocolatebar:
+		soundChocolatebar.play()
+		
 	#Starts Globaltimer for effect end
 	GlobalSignals.timerFirerate.start(effectLength)
+	
+	# wait until sound is finished 
+	if soundChocolatebar:
+		await soundChocolatebar.finished
 	
 	#This Scene is done
 	queue_free()
--- Drops/drop_energydrink.gd
+++ Drops/drop_energydrink.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var sound := $AudioStreamPlayer
+
+
 var effectLength = 3
 
 func _ready():
@@ -9,8 +12,16 @@ func pickup(player : Node2D):
 	#Calls Player Method to speed up
 	player.speed_up(100)
 	
+	# plays Sound when picked up
+	if sound:
+		sound.play()
+		
 	#Starts Globaltimer for effect end
 	GlobalSignals.timerSpeedUp.start(effectLength)
 	
+	# wait until sound is finished 
+	if sound:
+		await sound.finished
+
 	#This Scene is done
 	queue_free()
