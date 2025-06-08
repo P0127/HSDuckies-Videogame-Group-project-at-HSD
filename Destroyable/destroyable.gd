b2d63cac1695bad0b31e7272c83a59ea5cc32f0f
Selina-Staender <selina.staender@outlook.de>
2025-06-08 14:13:30 +0200

    Destroyable objects added
    2 types, trashcans have randomized effects and types

---
+++ Destroyable/destroyable.gd
@@ -0,0 +1,46 @@
+extends StaticBody2D
+
+var hitpoints = 3 #hits required to destroy and spawn item
+
+#Different Trashcan styles
+var paper : Texture2D = preload("res://Destroyable/Destroyable_assets/Trashcan_Paper.png")
+var plastic : Texture2D = preload("res://Destroyable/Destroyable_assets/Trashcan_Plastic.png")
+var waste : Texture2D = preload("res://Destroyable/Destroyable_assets/Trashcan_Waste.png")
+
+var paper_effect : Texture2D = preload("res://assets/effects/Trashcan_paper.png")
+var plastic_effect : Texture2D = preload("res://assets/effects/Trashcan_plastic.png")
+var waste_effect : Texture2D = preload("res://assets/effects/Trashcan_waste.png")
+
+var trash_types = [paper, plastic, waste]
+var trash_types_effect = [paper_effect, plastic_effect, waste_effect]
+
+func _ready():
+	randomize()
+	var randomIndex : int = randi() % trash_types.size()  #Liefert Zufallszahl zwischen 0 und 2
+	if randomIndex <= trash_types.size():
+		$Sprite2D.texture = trash_types[randomIndex]
+	if randomIndex <= trash_types_effect.size():
+		$Trash_Explosion.texture = trash_types_effect[randomIndex]
+
+func take_damage():
+	hitpoints -= 1
+	
+	match (hitpoints):
+		2: 
+			$Sprite2D/AnimationPlayer.stop()
+			$Sprite2D/AnimationPlayer.play("hit")
+			$HitParticle_1.set_deferred("emitting", true)
+		1:
+			$Sprite2D/AnimationPlayer.stop()
+			$Sprite2D/AnimationPlayer.play("hit")
+			$HitParticle_2.set_deferred("emitting", true)
+		0:
+			$Sprite2D/AnimationPlayer.stop()
+			$Sprite2D/AnimationPlayer.play("destroyed")
+			$CollisionShape2D.set_deferred("disabled", true)
+			GlobalSignals.drop_item.emit(global_position)
+
+
+
+func destroy():
+	queue_free()
