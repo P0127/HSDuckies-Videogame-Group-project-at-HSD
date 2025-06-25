61c560b7ddf5940e32f7e94ddbecffcc1f16be86
karin0127 <kpurginova@gmail.com>
2025-06-25 23:44:11 +0200

    CreditScreen functional
    made the credit screen, it works the links work as well used the BBcode in rich table for looks and the same signals as game over basically for the logic

---
+++ hud/credits_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+@onready var back_b_anim = $BackButton/AnimatedSprite2D
+
+
+
+func _ready() -> void:
+	$VScrollBar/creditsText.meta_clicked.connect(_on_meta_clicked)
+	
+func _on_meta_clicked(meta):
+	OS.shell_open(meta)
+
+	
+
+func _on_back_button_pressed() -> void:
+	back_b_anim.play("pressed")
+	
+
+	GlobalSignals.scene_controller.remove_scene()
+	GlobalSignals.scene_controller.change_gui_scene("res://Hud/startscreen.tscn")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	GlobalSignals.scene_controller.remove_scene()
+	GlobalSignals.scene_controller.change_gui_scene("res://Hud/startscreen.tscn")
